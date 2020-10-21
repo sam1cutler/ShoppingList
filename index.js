31de387d5e43929fb54e1d464f5430d30bf03ec2
Sam Cutler <sam.cutler@gmail.com>
2020-10-21 14:37:53 -0700

    first major update with JS pseudocode

---
+++ index.js
@@ -0,0 +1,32 @@
+// Define Function #1: addItemFunc
+function addItemFunc() {
+    console.log('addItemFunc ran, for real.');
+}
+
+
+// Define Function #2: checkItemFunc
+function checkItemFunc() {
+    console.log('checkItemFunc ran.')
+}
+
+
+
+// Define Function #3: deleteItemFunc
+function deleteItemFunc() {
+    console.log('deleteItemFunc ran.')
+}
+
+
+
+
+// Define Function #4: runAllFuncs
+
+function runAllFuncs() {
+    addItemFunc();
+    checkItemFunc();
+    deleteItemFunc();
+}
+
+$(runAllFuncs);
+
+console.log('test');
