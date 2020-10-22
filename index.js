0f1ceaa0a3ff3972a36064c4e3254e73bb073b19
Sam Cutler <sam.cutler@gmail.com>
2020-10-21 17:13:27 -0700

    all functionality seems good

--- index.js
+++ index.js
@@ -1,32 +1,59 @@
 // Define Function #1: addItemFunc
 function addItemFunc() {
-    console.log('addItemFunc ran, for real.');
-}
+
+    $('form').submit(function(event) {
+        event.preventDefault();
+
+        // get submission data
+        const newItem = $(this).find('input[name="shopping-list-entry"]').val();
+
+        // .append(whole big thing)
+        $('.shopping-list').append(`
+            <li>
+            <span class="shopping-item">${newItem}</span>
+            <div class="shopping-item-controls">
+                <button class="shopping-item-toggle">
+                <span class="button-label">check</span>
+                </button>
+                <button class="shopping-item-delete">
+                <span class="button-label">delete</span>
+                </button>
+            </div>
+            </li>
+        `);
+        
+        // clear input field
+        $(this).find('input').val('');
+
+    })
+
+};
 
 
 // Define Function #2: checkItemFunc
 function checkItemFunc() {
-    console.log('checkItemFunc ran.')
-}
-
-
+    $('ul').on('click', '.shopping-item-toggle', function(event) {       
+        const targetItem = $(this).parents('li').find('.shopping-item');
+        targetItem.toggleClass('shopping-item__checked');
+    });
+};
 
 // Define Function #3: deleteItemFunc
 function deleteItemFunc() {
-    console.log('deleteItemFunc ran.')
-}
+    $('ul').on('click', '.shopping-item-delete', function(event) {
+        const targetItem = $(this).parents('li');
+        targetItem.remove();
+    })
+};
 
 
 
 
 // Define Function #4: runAllFuncs
-
 function runAllFuncs() {
     addItemFunc();
     checkItemFunc();
     deleteItemFunc();
-}
+};
 
 $(runAllFuncs);
-
-console.log('test');
